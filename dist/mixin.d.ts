7645027da756d2815cd11f0706e79731a8b7fa0b
Viktor <zhokhov@moevideo.com>
2019-08-21 15:32:04 +0300

    - publish new version

--- dist/mixin.d.ts
+++ dist/mixin.d.ts
@@ -4,6 +4,6 @@ export declare function mixin<Name extends string, Config extends AnyObject>(con
     mixinName: Name;
     init?: (this: Mixin<Name, Config>) => void;
 }): Mixin<Name, Config>;
-export declare function applyMixinsForClass<Mixins extends Array<Mixin<string, AnyObject>>, T extends Constructor<AnyObject>>(targetClass: T, ...mixins: Mixins): Constructor<InstanceType<T> & IUseMixins<Mixins>>;
+export declare function applyMixinsForClass<Mixins extends Array<Mixin<string, AnyObject>>, T extends Constructor<AnyObject>>(targetClass: T, ...mixins: Mixins): Constructor<InstanceType<T> & IUseMixins<Mixins>> & T;
 export declare function applyMixinsForObject<T extends AnyObject, Mixins extends Array<Mixin<string, AnyObject>>>(target: T, ...mixins: Mixins): T & IUseMixins<Mixins>;
 export declare function applyMixins<T extends AnyObject, Mixins extends Array<Mixin<string, AnyObject>>>(target: T, mixins: Mixins): void;
