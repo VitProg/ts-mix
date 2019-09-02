7a060dea8106d3c64b5fd5016eda523bf610c3bc
Viktor <zhokhov@moevideo.com>
2019-09-02 13:00:41 +0300

    - publish new version

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,4 +1,4 @@
-export { IUseMixins, Mixin, MixinsProp, WithMixin, MixinTarget } from './types';
+export { IUseMixins, Mixin, MixinFull, MixinThis, IMixinBase, MixinsProp, WithMixin, MixinTarget } from './types';
 export { MergeAll, UnionToIntersection, ArrayValues, MergeOmit } from './common.types';
 export { use, useProxy } from './decorators';
 export { haveMixins, haveMixin } from './type-guards';
