413bff3a3682ea820b834f5e26e1855afa074b6e
Viktor Zhokhov <zhokhov@moevideo.com>
2019-12-17 15:25:25 +0300

    rollup

--- dist/type-guards.d.ts
+++
@@ -1,4 +0,0 @@
-import { IMixinBase, IUseMixins, Mixin, WithMixin } from "./types";
-export declare function haveMixin<M extends Mixin<any, any>>(v: any, mixin: M): v is WithMixin<M>;
-export declare function haveMixins<Mixins extends Array<Mixin<any, any>>>(v: any, ...mixins: Mixins): v is IUseMixins<Mixins>;
-export declare function isMixin<M extends IMixinBase<any> = IMixinBase<any>>(value: any): value is M;
